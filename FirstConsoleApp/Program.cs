5785c85a8227a3b27970197f2bb3eea679375c11
user <user@HOMEC682>
2021-12-03 17:19:56 -0600

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Essential Training Part 1");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            //this is a change
+        }
+    }
+}
